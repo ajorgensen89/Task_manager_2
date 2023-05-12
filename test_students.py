5d4338c0abb0518e21e30f7412a44136386786a5
ajorgensen89 <ajorgensen89@gmail.com>
2023-05-12 13:12:35 +0000

    Student exercise

---
+++ test_students.py
@@ -0,0 +1,45 @@
+import unittest
+from student import Student
+from datetime import timedelta
+
+
+class TestStudent(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        print("setUpClass")
+
+    def setUp(self):
+        print("setUp")
+        self.student = Student("John", "Doe")
+
+    @classmethod
+    def tearDownClass(cls):
+        print("tearDownClass")
+
+    def tearDown(self):
+        print("tearDown")
+
+    def test_full_name(self):
+        print("test_full_name")
+        self.assertEqual(self.student.full_name, "John Doe")
+
+    def test_alert_santa(self):
+        print("test_alert_santa")
+        self.student.alert_santa()
+        self.assertTrue(self.student.naughty_list)
+
+    def test_email(self):
+        print("test_email")
+        self.assertEqual(self.student.email, "john.doe@email.com")
+
+    def test_apply_extension(self):
+        old_end_date = self.student.end_date
+        self.student.apply_extension(5)
+
+        self.assertEqual(
+            self.student.end_date, old_end_date + timedelta(days=5))
+
+
+if __name__ == "__main__":
+    unittest.main()
